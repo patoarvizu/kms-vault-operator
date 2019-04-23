28cd4f5be689b607e7ad9e365290ac9a812435e5
patoarvizu <patoarvizu@gmail.com>
2019-04-22 22:51:44 -0400

    Exctract setup logic into its own function

--- test/e2e/kmsvaultoperator_test.go
+++ test/e2e/kmsvaultoperator_test.go
@@ -9,6 +9,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 
 	vaultapi "github.com/hashicorp/vault/api"
+	"github.com/operator-framework/operator-sdk/pkg/test"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
 	apis "github.com/patoarvizu/kms-vault-operator/pkg/apis"
@@ -16,14 +17,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestKMSVaultSecretV1(t *testing.T) {
-	ctx := framework.NewTestCtx(t)
-	defer ctx.Cleanup()
-	namespace, err := ctx.GetNamespace()
-	if err != nil {
-		t.Fatalf("could not get namespace: %v", err)
-	}
-
+func setup(t *testing.T, ctx *test.TestCtx) {
 	awsSecret := &v1.Secret{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Secret",
@@ -31,23 +25,19 @@ func TestKMSVaultSecretV1(t *testing.T) {
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "aws-secrets",
-			Namespace: namespace,
+			Namespace: "default",
 		},
 		StringData: map[string]string{
 			"AWS_ACCESS_KEY_ID":     os.Getenv("AWS_ACCESS_KEY_ID"),
 			"AWS_SECRET_ACCESS_KEY": os.Getenv("AWS_SECRET_ACCESS_KEY"),
 		},
 	}
-
 	framework.Global.Client.Create(context.TODO(), awsSecret, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
-
 	ctx.InitializeClusterResources(&framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
-
-	err = e2eutil.WaitForOperatorDeployment(t, framework.Global.KubeClient, namespace, "kms-vault-operator", 1, time.Second*5, time.Second*30)
+	err := e2eutil.WaitForOperatorDeployment(t, framework.Global.KubeClient, "default", "kms-vault-operator", 1, time.Second*5, time.Second*30)
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	kmsVaultSecretList := &operator.KMSVaultSecretList{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "KMSVaultSecret",
@@ -58,6 +48,12 @@ func TestKMSVaultSecretV1(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to add to scheme: %s", err)
 	}
+}
+
+func TestKMSVaultSecretV1(t *testing.T) {
+	ctx := framework.NewTestCtx(t)
+	defer ctx.Cleanup()
+	setup(t, ctx)
 
 	secret := &operator.KMSVaultSecret{
 		TypeMeta: metav1.TypeMeta{
@@ -66,7 +62,7 @@ func TestKMSVaultSecretV1(t *testing.T) {
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "test-secret",
-			Namespace: namespace,
+			Namespace: "default",
 		},
 		Spec: operator.KMSVaultSecretSpec{
 			Path:            "secret/test-secret",
@@ -83,14 +79,14 @@ func TestKMSVaultSecretV1(t *testing.T) {
 		},
 	}
 
-	framework.Global.Client.Create(context.TODO(), secret, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
+	err := framework.Global.Client.Create(context.TODO(), secret, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
 	if err != nil {
 		t.Fatalf("failed to create secret: %v", err)
 	}
 
 	time.Sleep(time.Second * 5)
 
-	vaultSecret, err := framework.Global.KubeClient.CoreV1().Secrets(namespace).Get("vault-unseal-keys", metav1.GetOptions{})
+	vaultSecret, err := framework.Global.KubeClient.CoreV1().Secrets("default").Get("vault-unseal-keys", metav1.GetOptions{})
 	if err != nil {
 		t.Fatalf("Failed to get Vault root token: %v", err)
 	}
@@ -123,45 +119,7 @@ func TestKMSVaultSecretV1(t *testing.T) {
 func TestKMSVaultSecretFinalizers(t *testing.T) {
 	ctx := framework.NewTestCtx(t)
 	defer ctx.Cleanup()
-	namespace, err := ctx.GetNamespace()
-	if err != nil {
-		t.Fatalf("could not get namespace: %v", err)
-	}
-
-	awsSecret := &v1.Secret{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Secret",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "aws-secrets",
-			Namespace: namespace,
-		},
-		StringData: map[string]string{
-			"AWS_ACCESS_KEY_ID":     os.Getenv("AWS_ACCESS_KEY_ID"),
-			"AWS_SECRET_ACCESS_KEY": os.Getenv("AWS_SECRET_ACCESS_KEY"),
-		},
-	}
-
-	framework.Global.Client.Create(context.TODO(), awsSecret, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
-
-	ctx.InitializeClusterResources(&framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
-
-	err = e2eutil.WaitForOperatorDeployment(t, framework.Global.KubeClient, namespace, "kms-vault-operator", 1, time.Second*5, time.Second*30)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	kmsVaultSecretList := &operator.KMSVaultSecretList{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "KMSVaultSecret",
-			APIVersion: "k8s.patoarvizu.dev/v1alpha1",
-		},
-	}
-	err = framework.AddToFrameworkScheme(apis.AddToScheme, kmsVaultSecretList)
-	if err != nil {
-		t.Fatalf("Failed to add to scheme: %s", err)
-	}
+	setup(t, ctx)
 
 	secret := &operator.KMSVaultSecret{
 		TypeMeta: metav1.TypeMeta{
@@ -170,7 +128,7 @@ func TestKMSVaultSecretFinalizers(t *testing.T) {
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "test-secret",
-			Namespace: namespace,
+			Namespace: "default",
 			Finalizers: []string{
 				"delete.k8s.patoarvizu.dev",
 			},
@@ -190,14 +148,14 @@ func TestKMSVaultSecretFinalizers(t *testing.T) {
 		},
 	}
 
-	framework.Global.Client.Create(context.TODO(), secret, nil)
+	err := framework.Global.Client.Create(context.TODO(), secret, nil)
 	if err != nil {
 		t.Fatalf("failed to create secret: %v", err)
 	}
 
 	time.Sleep(time.Second * 5)
 
-	vaultSecret, err := framework.Global.KubeClient.CoreV1().Secrets(namespace).Get("vault-unseal-keys", metav1.GetOptions{})
+	vaultSecret, err := framework.Global.KubeClient.CoreV1().Secrets("default").Get("vault-unseal-keys", metav1.GetOptions{})
 	if err != nil {
 		t.Fatalf("Failed to get Vault root token: %v", err)
 	}
